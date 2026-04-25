1f3c1b92caa416ecaaa111979db80169ce98eb6a
qa-schema <andreas.testing.qa@gmail.com>
2026-04-25 22:29:04 +0200

    Update test_login_error_screenshot.py

--- automation/test_login_error_screenshot.py
+++ automation/test_login_error_screenshot.py
@@ -2,31 +2,38 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import time
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def test_invalid_login_shows_error_and_screenshot():
-    from selenium.webdriver.chrome.options import Options
+    options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
 
-options = Options()
-options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-
-driver = webdriver.Chrome(options=options)
-    driver.get("https://example.com/login")  # TODO: echte Seite einsetzen
+    driver = webdriver.Chrome(options=options)
 
     try:
-        username_input = driver.find_element(By.ID, "username")
+        driver.get("https://the-internet.herokuapp.com/login")
+
+        wait = WebDriverWait(driver, 10)
+
+        username_input = wait.until(
+            EC.presence_of_element_located((By.ID, "username"))
+        )
         password_input = driver.find_element(By.ID, "password")
 
         username_input.send_keys("wrong_user")
         password_input.send_keys("wrong_password")
         password_input.send_keys(Keys.RETURN)
 
-        time.sleep(2)
-
-        error_message = driver.find_element(By.ID, "error")
+        error_message = wait.until(
+            EC.presence_of_element_located((By.ID, "flash"))
+        )
 
         assert "invalid" in error_message.text.lower()
 
