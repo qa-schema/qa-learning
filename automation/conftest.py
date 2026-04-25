73b394920ebc7125860a858c963ef3800f3bf119
qa-schema <andreas.testing.qa@gmail.com>
2026-04-25 23:07:02 +0200

    Update conftest.py

--- automation/conftest.py
+++ automation/conftest.py
@@ -1,23 +1,22 @@
 import os
-import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import pytest
 
 
 @pytest.fixture
 def driver():
     options = Options()
 
-    # Если переменная HEADLESS=1 (например в CI)
     if os.getenv("HEADLESS") == "1":
         options.add_argument("--headless=new")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-gpu")          # 👈 ДОБАВИТЬ
+        options.add_argument("--window-size=1920,1080") # 👈 ДОБАВИТЬ
 
     driver = webdriver.Chrome(options=options)
-    driver.maximize_window()
 
     yield driver
 
     driver.quit()
-
