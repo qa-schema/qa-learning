d8d0006c8521b070821dce12026ba6fd3266c048
qa-schema <andreas.testing.qa@gmail.com>
2026-04-27 22:04:19 +0200

    Remove Chrome binary location from driver setup
    Removed binary location setting for Chrome driver.

--- automation/conftest.py
+++ automation/conftest.py
@@ -8,15 +8,13 @@ from selenium.webdriver.chrome.options import Options
 def driver():
     options = Options()
 
+    # Headless для CI (GitHub Actions)
     if os.getenv("HEADLESS") == "1":
         options.add_argument("--headless=new")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--disable-gpu")
         options.add_argument("--window-size=1920,1080")
-
-
-    options.binary_location = "/usr/bin/google-chrome"
 
     driver = webdriver.Chrome(options=options)
 
